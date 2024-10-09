1f8e33fefd8618b43a8214ad7ca923b6326ccb8a
pedro-redraw <pedro.rosa@redraw.pro>
2024-10-09 14:04:44 -0300

    Add  login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <h1 className="text-4xl font-bold text-center">Welcome to Vestiq</h1>
-     <a
-     href="/sign-up"
-     >cadastre-se</a>
-    </div>
-  );
-}
+export default function Login() {
+    return (
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+       <h1 className="text-4xl font-bold text-center">Welcome to Vestiq</h1>
+       <a
+       href="/login"
+       >Fazer Login</a>
+      </div>
+    );
+  }
---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+export default function SignUp() {
+    return (
+      <div>
+        <h1>Sign Up</h1>
+        <a
+        href="/login"
+        > voltar para login</a>
+      </div>
+    );
+  }
+  
