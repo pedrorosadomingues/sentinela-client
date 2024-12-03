660d6630af22650a277f5032132fdd643c6e7e76
pedro-redraw <pedro.rosa@redraw.pro>
2024-12-03 20:56:24 -0300

    feat: lock sidebar button, coins icon and coinsModal

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -9,9 +9,13 @@ import CheckroomIcon from "@mui/icons-material/Checkroom";
 import HistoryIcon from "@mui/icons-material/History";
 import { useMainStore, useImageFunctionStore } from "@/zustand-stores";
 import { Divider } from "@nextui-org/react";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 
 export default function Sidebar(): JSX.Element {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [isLocked, setIsLocked] = useState(false);
+  const [openCoinModal, setOpenCoinModal] = useState(false);
 
   const { setMainControl, mainControl } = useMainStore();
   const { imageFunctions, getImageFunctions } = useImageFunctionStore();
@@ -71,15 +75,32 @@ export default function Sidebar(): JSX.Element {
     }
   }, [getImageFunctions]);
 
+  const toggleLock = () => {
+    setIsLocked((prev) => !prev);
+    setIsExpanded((prev) => !prev);
+  };
+
+  const handleMouseEnter = () => {
+    if (!isLocked) {
+      setIsExpanded(true);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (!isLocked) {
+      setIsExpanded(false);
+    }
+  };
+
   return (
     <aside
-      onMouseEnter={() => setIsExpanded(true)}
-      onMouseLeave={() => setIsExpanded(false)}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       className={`select-none fixed h-screen left-0 z-40 transition-all duration-700 ease-smooth-return-end 
-        ${isExpanded ? "w-[281px]" : "w-20"} 
+        ${isExpanded ? "w-[251px]" : "w-20"} 
         bg-white border-r border-gray-200`}
     >
-      <div className="p-4 flex flex-col items-center">
+      <div className="p-4 flex flex-col items-center relative h-full">
         {isExpanded ? (
           <Image
             src={"/images/logo-vestiq.png"}
@@ -99,71 +120,103 @@ export default function Sidebar(): JSX.Element {
             priority={true}
           />
         )}
+        <div className="flex flex-col justify-between h-full">
+          <ul
+            className={`mt-4 w-full flex flex-col text-[#565D6DFF] gap-[10px] items-${
+              isExpanded ? `start` : `center`
+            }`}
+          >
+            {MAIN_ITEMS.map((item) => (
+              <li
+                key={item.name}
+                className={`mb-2 flex items-center justify-center md:justify-start hover:text-[#F10641] w-full group hover:cursor-pointer rounded-lg p-2 min-w-[50px]
+                   ${
+                     mainControl === item.name
+                       ? "text-[#F10641] bg-[#FED2DD]"
+                       : ""
+                   }`}
+                onClick={() => setMainControl(item.name)}
+              >
+                {item.icon_path}
+                {isExpanded && (
+                  <span className="ml-3 overflow-hidden whitespace-nowrap group-hover:text-[#F10641]">
+                    {item.name}
+                  </span>
+                )}
+              </li>
+            ))}
 
-        <ul
-          className={`mt-4 w-full flex flex-col text-[#565D6DFF] gap-[10px] items-${
-            isExpanded ? `start` : `center`
-          }`}
-        >
-          {MAIN_ITEMS.map((item) => (
-            <li
-              key={item.name}
-              className={`mb-2 flex items-center justify-center md:justify-start hover:text-[#F10641] w-full group hover:cursor-pointer rounded-lg p-2 min-w-[50px]
-                 ${
-                   mainControl === item.name
-                     ? "text-[#F10641] bg-[#FED2DD]"
-                     : ""
-                 }`}
-              onClick={() => setMainControl(item.name)}
-            >
-              {item.icon_path}
-              {isExpanded && (
-                <span className="ml-3 overflow-hidden whitespace-nowrap group-hover:text-[#F10641]">
-                  {item.name}
-                </span>
+            <Divider className="w-2/3 group-hover:w-[100%] mx-auto" />
+
+            {imageFunctions &&
+              imageFunctions.map(
+                (func: {
+                  id: string;
+                  name: keyof typeof ICON_MAPPING;
+                  title: string;
+                }) => (
+                  <li
+                    key={func.id}
+                    className={`mb-2 flex items-center justify-center md:justify-start hover:text-[#F10641] w-full group hover:cursor-pointer rounded-lg p-2 min-w-[50px]
+                      ${
+                        mainControl === text(func.name)
+                          ? "text-[#F10641] bg-[#FED2DD]"
+                          : ""
+                      }`}
+                    onClick={() => {
+                      setMainControl(text(func.name));
+                    }}
+                  >
+                    {ICON_MAPPING[func.name] || (
+                      <FaInfoCircle
+                        className={`text-xl ${!isExpanded && "m-auto"}`}
+                      />
+                    )}
+                    {isExpanded && (
+                      <span className="ml-3 overflow-hidden whitespace-nowrap">
+                        {func.title}
+                      </span>
+                    )}
+                  </li>
+                )
               )}
-            </li>
-          ))}
-
-          <Divider className="w-2/3 group-hover:w-[100%] mx-auto" />
-
-          {imageFunctions &&
-            imageFunctions.map(
-              (func: {
-                id: string;
-                name: keyof typeof ICON_MAPPING;
-                title: string;
-              }) => (
-                <li
-                  key={func.id}
-                  className={`mb-2 flex items-center justify-center md:justify-start hover:text-[#F10641] w-full group hover:cursor-pointer rounded-lg p-2 min-w-[50px]
-                    ${
-                      mainControl === text(func.name)
-                        ? "text-[#F10641] bg-[#FED2DD]"
-                        : ""
-                    }`}
-                  onClick={() => {
-                    setMainControl(text(func.name));
-                  }}
-                >
-                  {ICON_MAPPING[func.name] || (
-                    <FaInfoCircle className="text-xl" />
-                  )}
-                  {isExpanded && (
-                    <span className="ml-3 overflow-hidden whitespace-nowrap">
-                      {func.title}
-                    </span>
-                  )}
-                </li>
-              )
-            )}
-        </ul>
-        <button
-          onClick={() => setIsExpanded((prev) => !prev)}
-          className="md:hidden absolute top-4 right-4 bg-black p-2 rounded-full z-50 text-black"
-        >
-          {isExpanded ? "Close" : "Open"}
-        </button>
+          </ul>
+          <div className="flex items-center justify-center flex-col">
+            <button
+              onClick={toggleLock}
+              className={`bg-transparent p-2 rounded-full z-50 text-gray-600 hover:text-gray-800 focus:outline-none flex items-center justify-center ${
+                isExpanded && "w-full"
+              }`}
+              title={isLocked ? "Fechar Sidebar" : "Abrir Sidebar"}
+            >
+              {!isLocked ? (
+                <KeyboardDoubleArrowLeftIcon />
+              ) : (
+                <KeyboardDoubleArrowRightIcon />
+              )}
+            </button>
+            <div
+              className="relative flex border border-gray-200 rounded-[10px] p-2 mt-4 gap-4"
+              onMouseEnter={() => isLocked && setOpenCoinModal(true)}
+              onMouseLeave={() => isLocked && setOpenCoinModal(false)}
+            >
+              <div
+                className={`absolute flex border border-gray-200 rounded-[10px] p-2 left-[45px] bottom-[2px] mb-[12px]
+                  ${openCoinModal ? "" : "hidden"}`}
+              >
+                {<span className="text-[#F10641]">0/1000</span>}
+              </div>
+              <Image
+                src="/icons/coins-icon.png"
+                alt="Logo"
+                width={25}
+                height={25}
+                priority={true}
+              />
+              {isExpanded && <span className="text-[#F10641]">0/1000</span>}
+            </div>
+          </div>
+        </div>
       </div>
     </aside>
   );
