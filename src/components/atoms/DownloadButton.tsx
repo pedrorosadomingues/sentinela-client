b31e1d85f30c9c64b56eb530b5954be4e2584a87
pedro-redraw <pedro.rosa@redraw.pro>
2025-01-22 17:27:56 -0300

    feat: enhance DownloadButton to manage download state and disable button during download

--- src/components/atoms/DownloadButton.tsx
+++ src/components/atoms/DownloadButton.tsx
@@ -1,12 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import React from "react";
 import { extractFileName } from "@/utils/extract-filename";
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
 import { createDownloadImage } from "@/services/download-image/create";
 
 export default function DownloadButton({ generation }: { generation: any }) {
+  const [isDownloading, setIsDownloading] = React.useState(false);
+
   async function fetchDownloadUrl() {
     const fileName = extractFileName(generation.path);
     const token = localStorage.getItem("token");
+
+    // Desabilita o botão antes de iniciar a requisição
+    setIsDownloading(true);
 
     try {
       const response = await fetch(
@@ -18,6 +24,7 @@ export default function DownloadButton({ generation }: { generation: any }) {
           },
         }
       );
+
       const data = await response.json();
 
       if (data.downloadUrl) {
@@ -36,6 +43,8 @@ export default function DownloadButton({ generation }: { generation: any }) {
         // Remover o link após o download
         document.body.removeChild(link);
         URL.revokeObjectURL(blobUrl);
+
+        // Chama o serviço para registrar o download
         createDownloadImage({
           generation_id: generation.id,
           user_id: generation.user_id,
@@ -43,13 +52,19 @@ export default function DownloadButton({ generation }: { generation: any }) {
       }
     } catch (error) {
       console.error("Erro ao obter URL de download:", error);
+    } finally {
+      // Habilita o botão novamente, seja sucesso ou erro
+      setIsDownloading(false);
     }
   }
 
   return (
     <button
       onClick={fetchDownloadUrl}
-      className="bg-blue-700 text-white rounded-md"
+      disabled={isDownloading} // desativa enquanto isDownloading for true
+      className={`bg-blue-700 text-white rounded-md ${
+        isDownloading ? "cursor-not-allowed" : "hover:bg-blue-800"
+      }`}
     >
       <FileDownloadIcon className="bg-blue-700 rounded" />
     </button>
