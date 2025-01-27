d634de8a86fc46f880153e75449ddd2b40c368b0
plfmoura-redraw <pedro.moura@redraw.pro>
2025-01-27 17:44:14 -0300

    style: add new layout for small devices

--- src/components/atoms/ChooseModelButton.tsx
+++ src/components/atoms/ChooseModelButton.tsx
@@ -4,7 +4,6 @@ import {
   ModalContent,
   ModalHeader,
   ModalBody,
-  ModalFooter,
   Button,
   useDisclosure,
   ScrollShadow,
@@ -13,7 +12,7 @@ import {
   Image,
   Textarea,
 } from "@heroui/react";
-import { useLocale, useTranslations } from "next-intl";
+import { useTranslations } from "next-intl";
 import { CloseOutlined } from "@mui/icons-material";
 import { useFormik } from "formik";
 
@@ -29,7 +28,6 @@ export default function ChooseModelButton({
   onModelSelect,
 }: ChooseModelButtonProps) {
   const t = useTranslations("choose_model_modal");
-  const locale = useLocale();
   const [customModelImage, setCustomModelImage] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [prompt, setPrompt] = useState<string | null>(null);
@@ -61,137 +59,148 @@ export default function ChooseModelButton({
     onSubmit: (values) => handleGenerateCustomModel(values),
   });
 
-  return (
-    <>
-      <Button onPress={onOpen} color="secondary" size="lg" radius="sm">
-        {t("choose_model_button")}
-      </Button>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange} size="2xl" className=" pb-4">
-        <ModalContent>
-          <ModalHeader>{t("title")}</ModalHeader>
-          <ModalBody className="flex-row gap-4 max-h-[500px]">
-            <aside className="basis-3/5 flex flex-col max-h-full gap-4">
-              <form
-                onSubmit={formik.handleSubmit}
-                className="flex flex-col gap-4"
-              >
-                <Textarea
-                  label={t("generate_label")}
-                  labelPlacement="outside"
-                  variant="bordered"
-                  radius="sm"
-                  placeholder={t("generate_placeholder")}
-                  className="w-full"
-                  maxRows={3}
-                  isDisabled={isLoading}
-                  id="prompt"
-                  name="prompt"
-                  onChange={formik.handleChange}
-                  value={formik.values.prompt}
-                />
+  const GenerateCustomModelContent = () => {
+    return (
+      <aside className="basis-4/5 sm:basis-3/5 flex flex-col max-h-full gap-4">
+        <form onSubmit={formik.handleSubmit} className="flex flex-col gap-4">
+          <Textarea
+            label={t("generate_label")}
+            labelPlacement="outside"
+            variant="bordered"
+            radius="sm"
+            placeholder={t("generate_placeholder")}
+            className="w-full"
+            maxRows={3}
+            isDisabled={isLoading}
+            id="prompt"
+            name="prompt"
+            onChange={formik.handleChange}
+            value={formik.values.prompt}
+          />
+          <Button
+            color="secondary"
+            radius="sm"
+            className="w-full"
+            isLoading={isLoading}
+            type="submit"
+          >
+            {t("generate_button")}
+          </Button>
+        </form>
+
+        {customModelImage ? (
+          <Card
+            className="m-0 p-0 group relative overflow-hidden w-full"
+            shadow="none"
+            radius="sm"
+          >
+            <div className="w-full h-full">
+              <Image
+                alt={`Image of custom model`}
+                className="object-cover w-full h-full"
+                src={customModelImage as string}
+              />
+              <CardFooter className="group-hover:-translate-y-0 transition-all ease-in-out translate-y-24 w-full px-2 items-center justify-between bg-black/70 py-2 absolute bottom-0 left-0 shadow-small z-10">
+                <p className="text-tiny text-white/80 line-clamp-1">{prompt}</p>
                 <Button
                   color="secondary"
+                  size="sm"
                   radius="sm"
-                  className="w-full"
-                  isLoading={isLoading}
-                  type="submit"
+                  onPress={() => handleSelectModel(customModelImage as string)}
                 >
-                  {t("generate_button")}
+                  {t("select_button")}
                 </Button>
-              </form>
-
-              {customModelImage ? (
-                <Card
-                  className="m-0 p-0 group relative overflow-hidden w-full"
-                  shadow="none"
-                  radius="sm"
-                >
-                  <div className="w-full h-full">
-                    <Image
-                      alt={`Image of custom model`}
-                      className="object-cover w-full h-full"
-                      src={customModelImage as string}
-                    />
-                    <CardFooter className="group-hover:-translate-y-0 transition-all ease-in-out translate-y-24 w-full px-2 items-center justify-between bg-black/70 py-2 absolute bottom-0 left-0 shadow-small z-10">
-                      <p className="text-tiny text-white/80 line-clamp-1">
-                        {prompt}
-                      </p>
-                      <Button
-                        color="secondary"
-                        size="sm"
-                        radius="sm"
-                        onPress={() =>
-                          handleSelectModel(customModelImage as string)
-                        }
-                      >
-                        {t("select_button")}
-                      </Button>
-                    </CardFooter>
-                    <Button
-                      isIconOnly
-                      size="sm"
-                      color="danger"
-                      className="absolute z-10 top-2 right-2"
-                      onPress={() => setCustomModelImage(null)}
-                    >
-                      <CloseOutlined fontSize="small" />
-                    </Button>
-                  </div>
-                </Card>
-              ) : (
-                <div className="h-full grid grid-cols-2 gap-4">
-                  {suggestions.map((suggestion) => (
-                    <div
-                      key={suggestion.id}
-                      className={`
+              </CardFooter>
+              <Button
+                isIconOnly
+                size="sm"
+                color="danger"
+                className="absolute z-10 top-2 right-2"
+                onPress={() => setCustomModelImage(null)}
+              >
+                <CloseOutlined fontSize="small" />
+              </Button>
+            </div>
+          </Card>
+        ) : (
+          <div className="h-full hidden xs:grid grid-cols-2 gap-4">
+            {suggestions.map((suggestion) => (
+              <div
+                key={suggestion.id}
+                className={`
                               ${
                                 isLoading
                                   ? "opacity-50 pointer-events-none cursor-not-allowed"
                                   : "opacity-100"
                               }
                               flex-1 h-full p-2 border border-gray-200 rounded-lg cursor-pointer select-none`}
-                      onClick={() =>
-                        formik.setFieldValue("prompt", suggestion.text)
-                      }
-                    >
-                      <p className="text-sm line-clamp-3">{suggestion.text}</p>
-                    </div>
-                  ))}
-                </div>
-              )}
-            </aside>
-            <ScrollShadow
-              as="aside"
-              className="pr-2 max-h-full w-full basis-3/5 scrollbar grid grid-cols-1 md:grid-cols-2 gap-4"
-              size={10}
-            >
-              {models.map((model) => (
-                <Card
-                  key={model.id}
-                  className="m-0 p-0 group relative overflow-hidden min-h-40"
-                  shadow="none"
-                  radius="sm"
-                >
-                  <Image
-                    alt={`Image of ${model.name}`}
-                    className="object-cover"
-                    height={200}
-                    src={model.image}
-                    width={200}
-                  />
-                  <CardFooter className="group-hover:-translate-y-0 transition-all ease-in-out translate-y-24 w-full px-2 items-center justify-between bg-black/70 py-2 absolute bottom-0 left-0 shadow-small z-10">
-                    <p className="text-tiny text-white/80">{model.name}</p>
-                    <Button
-                      color="secondary"
-                      size="sm"
-                      onPress={() => handleSelectModel(model.image)}
-                    >
-                      {t("select_button")}
-                    </Button>
-                  </CardFooter>
-                </Card>
-              ))}
-            </ScrollShadow>
+                onClick={() => formik.setFieldValue("prompt", suggestion.text)}
+              >
+                <p className="text-sm line-clamp-3">{suggestion.text}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </aside>
+    );
+  };
+
+  const SelectRedrawModels = () => {
+    return (
+      <ScrollShadow
+        as="aside"
+        className="xs:pr-2 max-h-60 xs:max-h-full basis-2/5 sm:basis-3/5 scrollbar grid grid-cols-2 xs:grid-cols-1 sm:grid-cols-2 gap-4"
+        size={10}
+      >
+        {models.map((model) => (
+          <Card
+            key={model.id}
+            className="m-0 p-0 group relative overflow-hidden min-h-32 w-full xs:min-h-40 xs:w-full"
+            shadow="none"
+            radius="sm"
+            onTouchStart={() => handleSelectModel(model.image)}
+          >
+            <Image
+              alt={`Image of ${model.name}`}
+              className="object-cover"
+              height={200}
+              src={model.image}
+              width={200}
+            />
+            <CardFooter className="group-hover:-translate-y-0 transition-all ease-in-out translate-y-24 w-full px-2 items-center justify-between bg-black/70 py-2 absolute bottom-0 left-0 shadow-small z-10">
+              <p className="hidden xs:block text-tiny text-white/80">
+                {model.name}
+              </p>
+              <Button
+                color="secondary"
+                size="sm"
+                onPress={() => handleSelectModel(model.image)}
+              >
+                {t("select_button")}
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </ScrollShadow>
+    );
+  };
+
+  return (
+    <>
+      <Button onPress={onOpen} color="secondary" size="lg" radius="sm">
+        {t("choose_model_button")}
+      </Button>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        size="2xl"
+        className=" pb-4"
+      >
+        <ModalContent>
+          <ModalHeader>{t("title")}</ModalHeader>
+          <ModalBody className="xs:flex-row gap-4 xs:max-h-[500px]">
+            <GenerateCustomModelContent />
+            <SelectRedrawModels />
           </ModalBody>
         </ModalContent>
       </Modal>
