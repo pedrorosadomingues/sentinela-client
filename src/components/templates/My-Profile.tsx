88d3a5952e9699d6785393bde15cb0696b687bde
pedro-redraw <pedro.rosa@redraw.pro>
2025-01-27 12:12:17 -0300

    feat: refactor profile menu components for improved organization and layout

--- src/components/templates/My-Profile.tsx
+++ src/components/templates/My-Profile.tsx
@@ -1,7 +1,8 @@
-import MenuProfile from "../organisms/my-profile/Menu-Profile";
+import MenuProfile from "../organisms/my-profile/MenuLeft";
 import PersonalData from "../organisms/my-profile/Personal-Data";
 import UserProfile from "../organisms/my-profile/User-Profile";
 import PlanBilling from "../organisms/my-profile/Plan-Billing";
+import MenuUp from "../organisms/my-profile/MenuUp";
 import { useMyProfileStore } from "@/zustand-stores";
 import { useTranslations } from "next-intl";
 
@@ -10,9 +11,10 @@ export default function MyProfile(): JSX.Element {
   const text = useTranslations("menu_profile");
 
   return (
-    <div className="bg-primary min-h-screen flex justify-start w-full items-start mt-[80px] ml-[80px]">
+    <div className="bg-primary min-h-screen flex justify-start w-[90%] items-start mt-[80px] ml-[80px] p-5 max765:ml-0">
       <MenuProfile />
       <div className="flex flex-col w-full">
+        <MenuUp />
         {profileControl === text("user_profile") && <UserProfile />}
         {profileControl === text("personal_data") && <PersonalData />}
         {profileControl === text("plan_billing") && <PlanBilling />}
