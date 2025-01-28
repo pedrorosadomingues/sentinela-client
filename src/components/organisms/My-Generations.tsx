22515a04c32cd0a49926a246ce12e2417b2bd18f
pedro-redraw <pedro.rosa@redraw.pro>
2025-01-28 18:48:24 -0300

    feat: pass loading state to GenerationCard for improved loading management

--- src/components/organisms/My-Generations.tsx
+++ src/components/organisms/My-Generations.tsx
@@ -24,7 +24,7 @@ export default function MyGenerations(): JSX.Element {
     <div className="animate-fade-in my-4 md:my-8 grid w-full 3xl:max-w-7xl mx-auto gap-4 grid-cols-1 xs:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-7 3xl:grid-cols-6">
       {generations &&
         generations.map((generation) => (
-          <GenerationCard key={generation.id} data={generation} />
+          <GenerationCard key={generation.id} data={generation} isLoading={isLoading} setIsLoading={setIsLoading} />
         ))}
     </div>
   );
