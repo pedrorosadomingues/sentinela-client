b221976585d953b7c49006453bdbe6c1d4bf05ca
plfmoura-redraw <pedro.moura@redraw.pro>
2025-01-29 20:16:00 -0300

    fix: unused user types

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -61,7 +61,7 @@ export default function Header(): JSX.Element {
     const stored_user_name = localStorage.getItem("user_name");
     const local_user = stored_user_id ? getUser(stored_user_id) : null;
 
-    if (!local_user || stored_user_name !== (user as User)?.name) {
+    if (!local_user || stored_user_name !== user?.name) {
       redirect(`/${locale}`);
     }
 
