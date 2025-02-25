48080f8c013e95b4d6c8875a9daf6cf0e14d8376
plfmoura-redraw <pedro.moura@redraw.pro>
2025-02-25 18:17:50 -0300

    refactor: simplify GET request handler by removing unused parameter

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
     console.log("📌 [API Next.js] Recebendo requisição GET /api/user");
 
