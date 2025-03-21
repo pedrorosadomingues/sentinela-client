8052476a73ebf67207d9b01850cfe15c452185f4
pedro-redraw <pedro.rosa@redraw.pro>
2025-03-21 01:30:09 -0300

    feat: add filtering functionality for generations based on date in FilterBtn and generationStore

--- src/stores/generationStore.tsx
+++ src/stores/generationStore.tsx
@@ -34,6 +34,8 @@ interface IGenerationStore {
   sortedGenerations: Generations[] | null;
   setSortedGenerations: (sortedGenerations: Generations[] | null) => void;
   sortGenerations: (order: "newest-editions" | "oldest-editions") => void;
+
+  filterGenerations: (filter: string) => void;
 }
 
 export const useGenerationStore = create<IGenerationStore>((set) => ({
@@ -178,8 +180,7 @@ export const useGenerationStore = create<IGenerationStore>((set) => ({
   },
 
   sortGenerations: (order: "newest-editions" | "oldest-editions") => {
-    const { generations, setSortedGenerations, sortedGenerations } =
-      useGenerationStore.getState();
+    const { generations, setSortedGenerations } = useGenerationStore.getState();
 
     if (generations) {
       const ssortedGenerations = [...generations].sort((a, b) => {
@@ -190,6 +191,52 @@ export const useGenerationStore = create<IGenerationStore>((set) => ({
       });
 
       setSortedGenerations(ssortedGenerations);
+    }
+  },
+
+  filterGenerations: (filter: string) => {
+    const { generations, setGenerations } = useGenerationStore.getState();
+    const now = new Date();
+  
+    const parseBRDate = (brDate: string) => {
+      const [datePart, timePart] = brDate.split(' ');
+      const [day, month, year] = datePart.split('/').map(Number);
+      const [hour, minute, second] = timePart.split(':').map(Number);
+  
+      return new Date(year, month - 1, day, hour, minute, second);
+    };
+  
+    const dateFilterMap: Record<string, Date> = {
+      today: new Date(now.getFullYear(), now.getMonth(), now.getDate()),
+      yesterday: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1),
+      'last-30-days': new Date(now.getFullYear(), now.getMonth(), now.getDate() - 30),
+      'last-90-days': new Date(now.getFullYear(), now.getMonth(), now.getDate() - 90),
+      'last-year': new Date(now.getFullYear() - 1, now.getMonth(), now.getDate()),
+      'any-time': new Date(0),
+    };
+  
+    const filterDate = dateFilterMap[filter.toLowerCase()];
+  
+    if (generations && filterDate) {
+      const filteredGenerations = generations.map((item) => {
+        const itemDate = parseBRDate(item.started_at);
+        let hidden = itemDate < filterDate;
+  
+        if (filter.toLowerCase() === 'yesterday') {
+          hidden =
+            itemDate < filterDate ||
+            itemDate >= new Date(now.getFullYear(), now.getMonth(), now.getDate());
+        } else if (filter.toLowerCase() === 'today') {
+          hidden = itemDate < filterDate;
+        }
+  
+        return {
+          ...item,
+          hidden,
+        };
+      });
+  
+      setGenerations(filteredGenerations);
     }
   },
 }));
